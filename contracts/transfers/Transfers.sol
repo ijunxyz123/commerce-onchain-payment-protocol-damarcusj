012ba49fc0a9e4ba7056444bea22cc365dc39bdd
ijunxyz123 <ijunxyz123@gmail.com>
2024-11-25 19:48:21 -0500

    Update Transfers.sol

--- contracts/transfers/Transfers.sol
+++ contracts/transfers/Transfers.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+contracts/transfers/Transfers.sol// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
-
+Damarcus Jones Sr 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
